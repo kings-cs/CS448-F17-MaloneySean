d77f8f4f239dcd3038d643bcd319339735ea1806
seanmaloney <seanmaloney@ad42527>
2017-09-21 18:35:38 -0400

    Implemented Graph Search.

--- CS448-F17-MalonySean/src/edu/kings/cs448/fall2017/MaloneySean/search/GraphSearch.java
+++ CS448-F17-MalonySean/src/edu/kings/cs448/fall2017/MaloneySean/search/GraphSearch.java
@@ -1,5 +1,7 @@
 package edu.kings.cs448.fall2017.MaloneySean.search;
 
+import java.util.HashMap;
+
 /**
  * Class that will solve a problem based on a Graph based search.
  * @author Sean Maloney
@@ -7,6 +9,13 @@ package edu.kings.cs448.fall2017.MaloneySean.search;
  * @param <A> The type of actions in the problem to solve.
  */
 public abstract class GraphSearch<S, A> extends SearchAlgorithm<S, A> {
+	
+	/**
+	 * The Set of states that have already been explored.
+	 */
+	private HashMap<S, SearchNode<S,A>> exploredSet;
+	
+
 	
 	/**
 	 * Abstract method to search the frontier for a node.
@@ -23,6 +32,54 @@ public abstract class GraphSearch<S, A> extends SearchAlgorithm<S, A> {
 	 * @return The node with the corresponding state.
 	 */
 	public SearchNode<S, A> searchExplored(S state) {
-		return null;
+		return exploredSet.get(state);
 	}
+
+	@Override
+	public void initializeExplored() {
+		exploredSet = new HashMap<S, SearchNode<S,A>>();
+	}
+
+	@Override
+	public void processChild(SearchNode<S, A> node) {
+		
+		SearchNode<S, A> exploredNode = exploredSet.get(node);
+		if(exploredSet == null) {
+			SearchNode<S, A> frontierNode = searchFrontier(node.getState());
+			if(frontierNode != null) {
+				//Is in the Frontier
+				int currentPathCost = frontierNode.getPathCost();
+				int newPathCost = node.getPathCost();
+				
+				if(newPathCost < currentPathCost) {
+					frontierNode.setParent(node.getParent());
+					frontierNode.setAction(node.getAction());
+					frontierNode.setPathCost(node.getPathCost());
+				}
+			}
+			else {
+				//In Neither Frontier Nor Explored
+				addToFrontier(node);
+			}
+		}
+		else {
+			//Is in the explored set
+			int currentPathCost = exploredNode.getPathCost();
+			int newPathCost = node.getPathCost();
+			
+			if(newPathCost < currentPathCost) {
+				exploredNode.setParent(node.getParent());
+				exploredNode.setAction(node.getAction());
+				exploredNode.setPathCost(node.getPathCost());
+			}
+		}		
+	}
+
+	@Override
+	public void addToExplored(SearchNode<S, A> node) {
+		S state = node.getState();
+		exploredSet.put(state, node);
+	}
+	
+
 }
