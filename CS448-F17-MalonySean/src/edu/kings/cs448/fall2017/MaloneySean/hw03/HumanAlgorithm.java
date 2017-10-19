45883947de4cbd0cfe331867783f5b7f8bf969d1
seanmaloney <seanmaloney@ad42523>
2017-10-19 15:29:33 -0400

    Created Human Algorithm class.

---
+++ CS448-F17-MalonySean/src/edu/kings/cs448/fall2017/MaloneySean/hw03/HumanAlgorithm.java
@@ -0,0 +1,24 @@
+package edu.kings.cs448.fall2017.MaloneySean.hw03;
+
+/**
+ * Class that allows a human to play a game through user input.
+ * @author Sean Maloney	
+ *
+ * @param <S> The kind of states used by the algorithm.
+ * @param <A> The kinds of actions used by the algorithm.
+ */
+public class HumanAlgorithm<S, A> implements StrategyAlgorithm<S, A> {
+
+	@Override
+	public A nextAction(StrategyGame<S, A> game, S state) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getStateCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
