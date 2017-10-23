1b25547af6697ea3d7464d3c9836dfa532851b7b
seanmaloney <seanmaloney@ad42523>
2017-10-23 18:21:43 -0400

    Created ConnectFour Main.

---
+++ CS448-F17-MalonySean/src/edu/kings/cs448/fall2017/MaloneySean/hw03/ConnectFourMain.java
@@ -0,0 +1,35 @@
+package edu.kings.cs448.fall2017.MaloneySean.hw03;
+
+/**
+ * A program for playing Connect Four.
+ * 
+ * @author Sean Maloney
+ */
+public class ConnectFourMain {
+	/**
+	 * The main method.
+	 * 
+	 * @param args Unused.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Let's play a game of Connect Four!");
+		
+		StrategyAlgorithm<ConnectFourState, ConnectFourAction> firstPlayer = GameUtilities.chooseAlgorithm(1);
+		StrategyAlgorithm<ConnectFourState, ConnectFourAction> secondPlayer = GameUtilities.chooseAlgorithm(2);
+		ConnectFourGame game = new ConnectFourGame();
+		ConnectFourState finalState = GameUtilities.playGame(game, firstPlayer, secondPlayer);
+
+		if(finalState.didPlayerWin(GamePlayer.MAX)) {
+			System.out.println(GamePlayer.MAX.getName() + " wins!");
+			System.out.println("Final Utility: " + game.getUtility(finalState));
+		}
+		else if(finalState.didPlayerWin(GamePlayer.MIN)) {
+			System.out.println(GamePlayer.MIN.getName() + " wins!");
+			System.out.println("Final Utility: " + game.getUtility(finalState));
+		}
+		else {
+			System.out.println("The game is a draw!");
+		}
+
+	}
+}
